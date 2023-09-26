835b8c5e33116198e7e75ff2df73704540687747
Omniladder <dustintobrien@gmail.com>
2023-09-26 09:35:42 -0400

    task 4 complete

--- COSC_350/Lab3/task4.c
+++ COSC_350/Lab3/task4.c
@@ -24,7 +24,7 @@ printf("Enter in the file you plan on copying to \n"); // prompts user
 scanf("%s" , outFileName); // reads in the file that data should be sent too
 
 
-outFile = open (outFileName, O_WRONLY|O_CREAT, 0770); //opens file used for outputting
+outFile = open (outFileName, O_WRONLY|O_CREAT, 0760); //opens file used for outputting
 
 while (read(inFile, &temp, 32) >= 1) // loops therought every charter in read file
 {
@@ -34,8 +34,10 @@ write(outFile, &temp, 32); //send charcter tot thhe write file
 printf("Enter in the file you plan on reading in \n"); //prompts user
 scanf("%s" , inFileName); // reads in the file name given by user
 
-lseek(outFileName, 0, SEEK_END);
+lseek(outFile, 0, SEEK_END);
 
+
+inFile = open(inFileName, O_RDONLY); //Opens the read file 
 
 while (read(inFile, &temp, 32) >= 1) // loops therought every charter in read file
 {
