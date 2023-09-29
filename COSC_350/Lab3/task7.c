3737a11a5235777d58acefbc5583f245f0d8fb94
Omniladder <dustintobrien@gmail.com>
2023-09-29 09:13:18 -0400

    Almost Finished task 7

---
+++ COSC_350/Lab3/task7.c
@@ -0,0 +1,68 @@
+//Fuck You Kyle
+
+
+#include <stdio.h>
+#include <string.h>
+
+int convert(char letter)
+{
+int convertedNum;
+if(letter == '0')
+{convertedNum = 0;}
+else  if(letter == '1')
+{convertedNum = 1;}
+else  if(letter == '2')
+{convertedNum = 2;}
+else  if(letter == '3')
+{convertedNum = 3;}
+else  if(letter == '4')
+{convertedNum = 4;}
+else  if(letter == '5')
+{convertedNum = 5;}
+else  if(letter == '6')
+{convertedNum = 6;}
+else  if(letter == '7')
+{convertedNum = 7;}
+else  if(letter == '8')
+{convertedNum = 8;}
+else  if(letter == '9')
+{convertedNum = 9;}
+else
+{
+printf ("ENTER NUMBERS IN ONLY \n"); 
+return -1;
+}
+}
+
+
+int main (int numOfNumbers, char* fullNum[])
+{
+int stringSize;
+int sum = 0;
+
+
+if(numOfNumbers <= 1)
+{
+printf("MUST INPUT AT LEAST 1 NUMBER PARAMETER \n");
+return -1;
+}
+
+
+for (int i = 1; i < numOfNumbers; i++)
+{
+    printf("%d", fullNum[i][0]);
+    stringSize = strlen(fullNum[i]);
+    for (int j = 0; j < stringSize; j++)
+    {
+        if (convert(fullNum[i][j]) != -1)
+            sum += convert(fullNum[i][j]);
+        else
+            return -1;
+}
+
+
+}
+
+printf("your final sum is %d\n",sum);
+
+}
