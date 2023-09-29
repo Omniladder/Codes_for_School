b9ec879698f19e16f9f5c39dc4f5ce7e8de0521b
Omniladder <dustintobrien@gmail.com>
2023-09-29 05:02:49 -0400

    Fixed Math 216 Name

--- COSC_350/Lab3/task5.c
+++ COSC_350/Lab3/task5.c
@@ -10,6 +10,8 @@ int main()
 int inFile, outFile; //holds File ID's for reading and writing too
 char temp; //stores one charcter for transfer
 
+umask(0000);
+
 char inFileName[256] = ""; // holds the file being read name
 char outFileName[256] = ""; // holds the file being copied too name
 
@@ -26,13 +28,13 @@ scanf("%s" , outFileName); // reads in the file that data should be sent too
 
 outFile = open (outFileName, O_WRONLY|O_CREAT, 0760); //opens file used for outputting
 
-int displace = -2;
+int displace = -2; //stores the offset of too move the cursor
 
-off_t fSize = lseek(inFile, -2, SEEK_END);
+off_t fSize = lseek(inFile, displace, SEEK_END); //sets cursor at the end of the file and stores the offset
 
-while ( (-1  * displace) - 3 < fSize) // loops therought every charter in read file
+while ( (-1  * displace) - 3 < fSize) // loops therought every charter in read file and makes sure we havent passed offset
 {
-read(inFile, &temp, 1);
+read(inFile, &temp, 1); //reads in the elements of the file given by displace
 write(outFile, &temp, 1); //send charcter tot thhe write file
 displace--;
 lseek(inFile, displace, SEEK_END);
