a60407f8c8020102c3060fe1c1ca087b02e75962
Dustin O'Brien <dustintobrien@gmail.com>
2023-10-02 10:58:54 -0400

    ADDED INPUT VALIDATION

--- COSC_350/Lab3/task3.c
+++ COSC_350/Lab3/task3.c
@@ -21,9 +21,21 @@ scanf("%s" , inFileName); // reads in the file name given by user
 
 inFile = open(inFileName, O_RDONLY); //Opens the read file 
 
+
+if (access(inFileName, F_OK) != 0)
+{
+        printf("FILE DOESNT EXIST");  
+        exit (1);
+}
+
+if (access(inFileName, R_OK) != 0)              
+{
+        printf("FILE ISN'T READABLE");
+        exit (1);
+}
+
 printf("Enter in the file you plan on copying to \n"); // prompts user
 scanf("%s" , outFileName); // reads in the file that data should be sent too
-
 
 outFile = open (outFileName, O_WRONLY|O_CREAT, 0770); //opens file used for outputting
 
