6efd7188fb7165a4e7e125bcb59630547914aa18
Dustin O'Brien <dustintobrien@gmail.com>
2023-10-23 14:49:46 -0400

    Better Task1

--- COSC_350/Lab6/task1.c
+++ COSC_350/Lab6/task1.c
@@ -6,52 +6,53 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-char* mygetenv(char* searchTerm); //prototypes function
+char* mygetenv(char* the_proper_term_to_be_identified_as_requested_by_the_user); //prototypes function
 
 extern char **environ; //gets in enviornment variables
-static char output[500];//used to store final result
+static char string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable[500];//used to store final string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable_two
 
 int main()
 {
-	char* result = mygetenv("PWD");//calls functions and stores result
-	if(result != NULL) //Checks if Input had been found
+	char* string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable_two = mygetenv("PWD");
+	//calls functions and stores string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable_two
+	if(string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable_two != NULL) //Checks if Input had been found
 	{
-		printf("%s", result);//outputs location
+		printf("%s", string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable_two);//outputs location
 	}
 }
 
-char* mygetenv(char* searchTerm)
+char* mygetenv(char* the_proper_term_to_be_identified_as_requested_by_the_user)
 {
-	int searchTermSize = strlen(searchTerm); //stores size of search term
-	int letterIndex = 0;//used to go through each letter of enviornment variable
+	int potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word_size = strlen(the_proper_term_to_be_identified_as_requested_by_the_user); //stores size of search term
+	int potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word = 0;//used to go through each letter of enviornment variable
 
 
 
-	for(int variableIndex = 0; environ[variableIndex] != NULL; variableIndex++) //loops through all enviornmental variables
+	for(int variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check = 0; environ[variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check] != NULL; variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check++) //loops through all enviornmental variables
 	{
 	
-		while(letterIndex < searchTermSize) //loops through every element of the term being ssearched for to figure out if its been found
+		while(potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word < potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word_size) //loops through every element of the term being ssearched for to figure out if its been found
 		{
-			if(environ[variableIndex][letterIndex] != searchTerm[letterIndex])//checks if any letters dont match if they dont breaks
+			if(environ[variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check][potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word] != the_proper_term_to_be_identified_as_requested_by_the_user[potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word])//checks if any letters dont match if they dont breaks
 			{
-				break;	//breaks out so that letterIndex would activate next one
+				break;	//breaks out so that potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word would activate next one
 			}
-			letterIndex++;//increments to next index
+			potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word++;//increments to next index
 		}
 		
 		
 		
-		if(letterIndex == searchTermSize && environ[variableIndex][searchTermSize] == '=') //checks if they were equal and that we hit end of word
+		if(potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word == potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word_size && environ[variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check][potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word_size] == '=') //checks if they were equal and that we hit end of word
 		{
-			strcpy(output, environ[variableIndex] + searchTermSize + 1);//sets output to variable
-			return strcat(output, "\n"); //adds \n to end and returns
+			strcpy(string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable, environ[variable_Used_To_Store_The_Location_Index_Of_A_Potential_Wanted_Enviornmental_Used_In_Order_Check] + potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word_size + 1);//sets string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable to variable
+			return strcat(string_used_to_store_the_variable_that_needs_to_be_outted_and_hold_teh_result_from_enviormental_variable, "\n"); //adds \n to end and returns
 		}
 		
-		letterIndex = 0; //starts letter index back at 0 for next one
+		potential_Index_of_letter_within_the_enviormental_variable_to_properly_check_if_one_by_one_the_letters_match_and_thus_the_word = 0; //starts letter index back at 0 for next one
 
 	}
 	
-	printf("FAILED TO FIND VARIABLE NAMED %s\n", searchTerm); //gives error if couldnt find term
+	printf("FAILED TO FIND VARIABLE NAMED %s\n", the_proper_term_to_be_identified_as_requested_by_the_user); //gives error if couldnt find term
 	
 	return (NULL);//used to tell if error happened
 }
