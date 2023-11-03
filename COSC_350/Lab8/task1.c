a705bd1e61fa124d097a7fade6fef8d28dfc852e
Omniladder <dustintobrien@gmail.com>
2023-11-03 01:02:05 -0400

    Finshed Task 1 Turned out to not be nearly as elegant as hoped void pointers suck and make it more messy

--- COSC_350/Lab8/task1.c
+++ COSC_350/Lab8/task1.c
@@ -4,54 +4,59 @@
 #include <string.h>
 
 
-int factorial(int);
-int sumnation(int);
+void* factorial(void*);
+void* sumnation(void*);
 
 int main(int numInputs, char* commandLine[])
 {
 	pthread_t factThread, sumThread;
 
 	int inputParameters[1];
-	inputParameters[0] = atoi(commandLine[0]);
+	inputParameters[0] = atoi(commandLine[1]);
 
-	if(pthread_create(&factThread, NULL, factorial, inputParameters) == 0) //WHY IS 0 ERROR
+	if(pthread_create(&factThread, NULL, factorial, inputParameters) != 0) 
 	{
 		printf("Thread 1 Failed");
 		return -1;
 	}
 
-	if(pthread_create(&sumThread, NULL, sumnation, inputParameters) == 0) //WHY IS 0 ERROR
+	if(pthread_create(&sumThread, NULL, sumnation, inputParameters) != 0)
 	{
 		printf("Thread 2 Failed");
 		return -1;
 	}
 
-	int* sumResult;
-	int* factResult;
+	int sumResult;
+	int factResult;
 	void* sumBuffer;
 	void* factBuffer;
 
-	pthread_join(sumThread, sumBuffer);	
-	pthread_join(factThread, factBuffer);	
 
-	sumResult = (int *)sumBuffer;
-	factResult = (int *)factBuffer;
 
-	printf("Final Sum Result %d", *sumResult);
-	printf("Final Factorial Result %d", *factResult);
+	pthread_join(sumThread, &sumBuffer);	
+	pthread_join(factThread, &factBuffer);	
+
+	
+	printf("Final Sum Result %d", sumResult);
+	printf("Final Factorial Result %d", factResult);
 
 }
 
 
-int factorial(int inputtedNum)
+void* factorial(void* inputtedNum)
 {
-	int sum = 1;
-	for(int i = 1; i <= inputtedNum; i++)
+	int sum;
+	sum = 1;
+	for(int i = 1; i <= *(int *)inputtedNum; i++)
 	{
 		sum *= i;
 	}
-	return sum;
+	return (void *)sum;
 }
 
-int sumnation(int num) //PARK WILL LOVE THIS
-{return (num + 1) * ((int) (num / 2)) + (num % 2) * ((num + 1) / 2);}
+void* sumnation(void* num) 
+{return (void *)(((* (int*) num + 1) * ((int)(*(int*)num) / 2)) + ((*(int*)num % 2) * (((*(int*)num + 1) / 2)))); }
+
+
+
+
