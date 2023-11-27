a0c797e7004856ff56096fefe20613eaaddb3c7d
Omniladder <dustintobrien@gmail.com>
2023-11-28 01:42:13 +0430

    FINISHED TASK 4

--- COSC_350/Lab9/pipeFIFO.c
+++ COSC_350/Lab9/pipeFIFO.c
@@ -19,9 +19,7 @@ buffer = (char *)calloc(strlen(some_data) + 1, sizeof(char));
 strcpy(buffer, some_data);
 
 buffer[strlen(some_data) + 1] = '\0';
-
-if ( (fileDescriptor = open("/tmp/task4_fifo", O_WRONLY)) > -1) {
-
+if ( (fileDescriptor = open("/tmp/task4_fifo", O_RDWR)) > -1) {
 	data_processed = write(fileDescriptor, buffer, strlen(some_data));
 	printf("Wrote %d bytes\n", data_processed);
 	data_processed = read(fileDescriptor, buffer, strlen(some_data));
