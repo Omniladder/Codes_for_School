21782f3af9d4dbc7ffaec304a51f65a1e50edef4
Omniladder <dustintobrien@gmail.com>
2024-04-03 02:10:20 -0400

    Begining Lab 3

--- COSC311/Lab3/source.py
+++ COSC311/Lab3/source.py
@@ -1,7 +1,29 @@
-import stats
+import stats as st
+import pandas as pd
+from matplotlib import pyplot as plt
 
-read_csv("")
+sidiBel = pd.read_csv("Sidi-Bel_Abbes_Region.csv")
+bejaia = pd.read_csv("Bejaia_Region.csv")
+
+#columnTitles = bejaia.columns
+
+print(type(bejaia))
+
+for i in bejaia.rows:
+    print(i)
+
+bejTemps = bejaia.iloc[:,3]
+bejHumid = bejaia.iloc[:,5]
+bejWind = bejaia.iloc[:,6]
+bejRain = bejaia.iloc[:,7]
+
+#fireBejTemprs = [i for i in bejTemps if ]
 
 
 
-print("Test")
+
+
+
+
+
+
