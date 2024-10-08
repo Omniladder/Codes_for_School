241b8dfbdc29f87ee51cf696ea492280000be35e
Omniladder <dustintobrien@gmail.com>
2024-10-08 00:11:37 -0400

    Reduced Functionality

---
+++ Cosc420/Exercise2/reduce.c
@@ -0,0 +1,57 @@
+#include "stdio.h"
+#include "mpi.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/*
+Sum of Maximums
+*/
+
+
+
+int main(int argc, char** argv){
+    //printf("Hello World\n");
+
+    MPI_Init(&argc, &argv);
+
+    int numComm = 0;
+    MPI_Comm_size(MPI_COMM_WORLD, &numComm);
+
+    int sum = 0;
+
+    int rank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int nums[10];
+       //Input Validation
+       sleep(rank);
+       srand(time(0));
+    for (int i = 0; i < 10; i++)
+    {
+        nums[i] = rand() % 10;
+    }
+
+    int max = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        if(max < nums[i])
+        {
+            max = nums[i];
+        }
+    }
+
+
+    int maxSum;
+    MPI_Reduce(&max, &maxSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(rank == 0)
+    {
+        printf("Sum of Random Maximums is %d\n", maxSum);
+    }
+      
+
+    
+    MPI_Finalize();
+}
